e575d4f3b82b17a1731d83a3124f30ee6a388283
mohammedalsamki <momosamak21@gmail.com>
2023-03-23 11:58:08 +0300

    add members deatels

--- members/views.py
+++ members/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 # Create your views here.
 
 # members function to return a HttpResponse
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render({}, request))
+
+    # get all membersd list
+    members_list = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'members_list': members_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+# detail function to return a member detail
+def details(request, member_id):
+    mymember = Member.objects.get(id=member_id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
 
 
-class person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# main function to return a home page 
+def main(request):
+    template = loader.get_template('main.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
 
-    def myfunc(self):
-        print("Hello my name is " + self.name)
 
 
-p1 = person("John", 36)
