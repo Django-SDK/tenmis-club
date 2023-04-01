66654bccf2ee0f15a9a6a327778ecebfa6d8e56a
mohammedalsamki <momosamak21@gmail.com>
2023-04-01 10:50:19 +0300

    added 404 page

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)8rhqw=d9gg&!^0z8+hvp+*ncsxdplak+od@h#*53sit)+@h^k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
